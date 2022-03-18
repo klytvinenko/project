13e6bccc818aecc96410445fd62c8b4224008168
Kristina <example@gmail.com>
2022-03-18 17:18:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//const answer = [];
+
+
+/*answer[0] = prompt("Як тебе звати?", " ");
+answer[1] = prompt("Як твоє прізвище?", " ");
+answer[2] = prompt("Який твій вік?", " ");
+
+
+document.write(answer); */
+
+const user = 'Ivan';
+
+alert(`Hello, ${user} !`);
