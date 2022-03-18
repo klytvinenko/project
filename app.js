8381a037a8c2378f822b914b0705192cafa3be57
Kristina <example@gmail.com>
2022-03-18 19:27:46 +0200

    Home_w0rk1

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ answer[2] = prompt("Який твій вік?", " ");
 
 document.write(answer); */
 
-const user = 'Ivan';
+const numberOfFilms = +prompt("Сколько фильмов вы ужее посмотрели?", " ");
 
-alert(`Hello, ${user} !`);
+const personalMovieDB = {
+
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt("Один из последних просмотреных фильмов?", ""),
+
+      b = prompt("На сколько оцените его?",""),
+      c = prompt("Один из последних просмотреных фильмов?", ""),
+      d = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
